f7d84c936628f140cea52beaea9b2cef7a1c65e7
Przemysław Sosna <przemek.sosna@onet.pl>
2019-12-18 18:44:11 +0100

    Added reading robots endpoint

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import datetime
+import json
+
+from bson import ObjectId
 from flask import Flask
+from flask.json import jsonify
+from flask_cors import CORS
 from flask_socketio import SocketIO
 
-from flask_cors import CORS
+from database import read_robots, create_new_robot
 
-from database import read_robots
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+
 
 app = Flask(__name__)
+app.json_encoder = JSONEncoder
 CORS(app)
 socketio = SocketIO(app, logger=True, engineio_logger=True, async_handlers=True)
 
@@ -17,7 +32,12 @@ def msg(msg):
 
 @socketio.on('read_all_robots')
 def read_all_robots_command(data):
-    return read_robots()
+    return JSONEncoder().encode(read_robots())
+
+
+@socketio.on('create_new_robot')
+def create_new_robot_command(robot):
+    create_new_robot(robot['name'], robot['key'], robot['robot_type'], robot['description'], robot['tags'])
 
 
 if __name__ == "__main__":
