b4361db7cf735d7c02036d18b9aa212fdd0a72b6
Przemysław Sosna <przemek.sosna@onet.pl>
2019-12-18 18:19:29 +0100

    Implemented basic database controllers
    (read all, add, etc)

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+from flask_cors import CORS
+
+from database import read_robots
+
 app = Flask(__name__)
-socketio = SocketIO(app)
+CORS(app)
+socketio = SocketIO(app, logger=True, engineio_logger=True, async_handlers=True)
 
 
 @socketio.on('message')
 def msg(msg):
     print('msg ', msg)
+
+
+@socketio.on('read_all_robots')
+def read_all_robots_command(data):
+    return read_robots()
 
 
 if __name__ == "__main__":
